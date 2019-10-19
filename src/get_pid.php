24f273264a8847730ca5faa51e1f255301c2889e
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-10-19 23:56:56 +0900

    特定のプロセスの終了を待って次の処理を実行したい(まだエラー中)

--- src/get_pid.php
+++ src/get_pid.php
@@ -2,7 +2,14 @@
 
 //バックグラウンドコマンドのプロセスIDを取得する
 
-$cmd = "sleep 30 > /dev/null & echo $!";
+$cmd_1 = "sleep 30 > /dev/null & echo $!";
 $output = array();
-exec($cmd, $output);
-echo $output[0];
+exec($cmd_1, $output);
+
+$pid = $output[0];
+echo $pid;
+
+$cmd_2 = "wait $pid ; echo line12 > /dev/null &";
+exec($cmd_2);
+
+echo "line 14";
